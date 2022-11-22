5a547cf34cb6bf834a37ffb2457ed0c8f51ba0a5
fredericojacobi <fredvjacobi@gmail.com>
2022-11-22 19:50:21 -0300

    feature: pagination model

---
+++ WebApi/Generics/Models/Pagination.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics.Models;
+
+public class Pagination<T> where T : class
+{
+    public IEnumerable<T> Records { get; }
+
+    public int PageSize { get; }
+
+    public int CurrentPage { get; }
+
+    public int NextPage => TotalPages > CurrentPage ? CurrentPage + 1 : CurrentPage;
+
+    public int PreviousPage => CurrentPage > 1 ? CurrentPage - 1 : CurrentPage;
+
+    public int LastPage => TotalPages;
+
+    public int TotalRecords { get; }
+
+    public int TotalPages => (int)Math.Ceiling((double)Records.Count() / PageSize);
+
+    public bool HasNextPage => NextPage > CurrentPage;
+
+    public bool HasPreviousPage => PreviousPage < CurrentPage;
+
+    public Pagination(IEnumerable<T> records, int totalRecords, int currentPage = 1, int pageSize = 10)
+    {
+        Records = records;
+        CurrentPage = currentPage;
+        TotalRecords = totalRecords;
+        PageSize = pageSize;
+    }
+}
